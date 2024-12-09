a4cea2f6b40332a4bdac074c8859877e0208a39e
daichi.sugiyama <caad12.di2@gmail.com>
2024-12-09 22:48:23 +0900

    登録ボタン設置 #27

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import BookRegisterButton from "./components/BookRegisterButton";
 import BookTable from "./components/BookTable";
 
 function App() {
   return (
     <>
       <h1>Artis Libri</h1>
+      <div className="book_register_btn">
+        <BookRegisterButton />
+      </div>
       <div className="card">
         <BookTable />
       </div>
