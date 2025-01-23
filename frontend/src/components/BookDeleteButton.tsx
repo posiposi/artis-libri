2703cfe10379a7f53e24d3de979fa65ad2e02fed
daichi.sugiyama <caad12.di2@gmail.com>
2025-01-23 20:17:01 +0900

    削除ダイアログモックを追加 #30

--- frontend/src/components/BookDeleteButton.tsx
+++ frontend/src/components/BookDeleteButton.tsx
@@ -1,13 +1,46 @@
 import { HStack } from "@chakra-ui/react";
 import { Button } from "@/components/ui/button";
 import { AiFillDelete } from "react-icons/ai";
+import {
+  DialogActionTrigger,
+  DialogBody,
+  DialogCloseTrigger,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogRoot,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 const BookDeleteButton = () => {
   return (
     <HStack>
-      <Button colorPalette="teal" variant="solid">
-        <AiFillDelete /> Edit
-      </Button>
+      <DialogRoot role="alertdialog">
+        <DialogTrigger asChild>
+          <Button variant="outline" size="sm">
+            <AiFillDelete /> Delete
+          </Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you sure?</DialogTitle>
+          </DialogHeader>
+          <DialogBody>
+            <p>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our systems.
+            </p>
+          </DialogBody>
+          <DialogFooter>
+            <DialogActionTrigger asChild>
+              <Button variant="outline">Cancel</Button>
+            </DialogActionTrigger>
+            <Button colorPalette="red">Delete</Button>
+          </DialogFooter>
+          <DialogCloseTrigger />
+        </DialogContent>
+      </DialogRoot>
     </HStack>
   );
 };
