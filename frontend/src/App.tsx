72c282fd122e54acb6f409d01f54687abfba7def
daichi.sugiyama <caad12.di2@gmail.com>
2025-02-02 17:55:08 +0900

    暫定実装完了 #50

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import BookTable from "./components/BookTable";
+import GetRecommendedBooksButton from "./components/GetRecommendedBooksButton";
 
 function App() {
   return (
     <>
       <h1>Artis Libri</h1>
       <div className="card">
+        <GetRecommendedBooksButton />
         <BookTable />
       </div>
     </>
---
+++ frontend/src/components/GetRecommendedBooksButton.tsx
@@ -0,0 +1,89 @@
+import { Button, Spinner, Center } from "@chakra-ui/react";
+import {
+  DialogActionTrigger,
+  DialogBody,
+  DialogCloseTrigger,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogRoot,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { AiFillBook } from "react-icons/ai";
+import { useState } from "react";
+
+const GetRecommendBooksButton = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [responseMessage, setResponseMessage] = useState<string>("");
+
+  const handleRecommend = async () => {
+    setLoading(true);
+    try {
+      const baseURL = import.meta.env.VITE_API_BASE_URL;
+      const response = await fetch(`${baseURL}/v1/books/recommend`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (!response.ok) {
+        throw new Error("error occurred");
+      }
+      const data = await response.json();
+      console.log(data);
+      setResponseMessage(data);
+      setLoading(false);
+    } catch (error: unknown) {
+      console.error(error);
+      setResponseMessage("error occurred");
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      {loading && (
+        <Center
+          position="fixed"
+          top="0"
+          left="0"
+          width="100vw"
+          height="100vh"
+          backgroundColor="rgba(0, 0, 0, 0.3)"
+        >
+          <Spinner size="xl" />
+        </Center>
+      )}
+      <DialogRoot>
+        <DialogTrigger asChild>
+          <Button variant="outline" size="sm" onClick={handleRecommend}>
+            {loading ? (
+              <Spinner size="sm" />
+            ) : (
+              <>
+                <AiFillBook /> Recommend
+              </>
+            )}
+          </Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Dialog Title</DialogTitle>
+          </DialogHeader>
+          <DialogBody>
+            <p>{responseMessage}</p>
+          </DialogBody>
+          <DialogFooter>
+            <DialogActionTrigger asChild>
+              <Button variant="outline">Close</Button>
+            </DialogActionTrigger>
+          </DialogFooter>
+          <DialogCloseTrigger />
+        </DialogContent>
+      </DialogRoot>
+    </>
+  );
+};
+
+export default GetRecommendBooksButton;
